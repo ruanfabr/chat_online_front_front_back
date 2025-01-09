da8110c025c499b7f33d7f9355b2a2da49021e45
ruan fab <victor.ruanfab@gmail.com>
2025-01-09 00:03:18 -0300

    :sparkles: feat(router): Adiciona rotas para as páginas de Join e Chat

---
+++ chatOnline_front/src/Routes.jsx
@@ -0,0 +1,7 @@
+import Join from './pages/Join/Join.jsx';
+import Chat from './pages/Chat/Chat.jsx';
+
+export {
+    Join,
+    Chat
+}
---
+++ chatOnline_front/src/pages/Join/Join.jsx
@@ -0,0 +1,29 @@
+import { useNavigate } from "react-router"
+import './join.css'
+
+
+export default function Join(){
+    
+
+    const go = useNavigate()
+
+    const handleClick = () => {
+        go('/chat')
+    }
+
+    return(
+        <div className="flex flex-col align-center w-[20rem] text-center space-y-5">
+            <h1 className="font-bold text-[20px] border-b-2 border-purple-800 flex self-center">Join</h1>
+
+            <div className="space-x-4 ">
+                <input type="text" name="nomeUser" id="" className="rounded-xl py-1 px-3 focus:outline-none focus:ring-1"/>
+                <button type="button" onClick={handleClick}
+                className="bg-gradient-to-t 
+                text-white font-medium py-1 px-2 rounded-md border-2 rainbow-animate"
+                >
+                    Enviar
+                </button>
+            </div>
+        </div>
+    )
+}
