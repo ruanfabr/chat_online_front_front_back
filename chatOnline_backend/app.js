ea8865e683a196ec53c29a3053f0d8aa1b41c2d0
ruan fab <victor.ruanfab@gmail.com>
2025-01-10 13:08:20 -0300

    :sparkles: feat(server): Adiciona suporte a WebSocket com Socket.IO no backend.

--- chatOnline_backend/app.js
+++ chatOnline_backend/app.js
@@ -1,4 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
 
-app.listen(8080, () => {console.log('api rodando na porta 8080')})
+const app = express()
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {origin: 'http://localhost:5173'}
+})
+
+server.listen(8080, () => console.log('api rodando na porta 8080'))
--- chatOnline_front/src/pages/Join/Join.jsx
+++ chatOnline_front/src/pages/Join/Join.jsx
@@ -11,14 +11,19 @@ export default function Join(){
     const handleClick = () => {
         go(('/chat/:id').replaceAll(':id', chatRoom))
     }
+    const enterKey = (e) => {
+        if (e.key == "Enter"){
+            go(('/chat/:id').replaceAll(':id', chatRoom))
+        }
+    }
 
     return(
         <div className="flex flex-col align-center w-[20rem] text-center space-y-5">
             <h1 className="font-bold text-[20px] border-b-2 border-purple-800 flex self-center">Join</h1>
 
             <div className="space-x-4 ">
-                <input type="text" name="nomeUser" id="" className="rounded-xl py-1 px-3 focus:outline-none focus:ring-1" onChange={(e) => {setChatRoom(e.target.value)}}/>
-                <button type="button" onClick={handleClick}
+                <input type="text" name="nomeUser" id="" className="rounded-xl py-1 px-3 focus:outline-none focus:ring-1" onChange={(e) => {setChatRoom(e.target.value)}} onKeyDown={enterKey}/>
+                <button type="submit" onClick={handleClick}
                 className="bg-gradient-to-t 
                 text-white font-medium py-1 px-2 rounded-md border-2 rainbow-animate"
                 >
